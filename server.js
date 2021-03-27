8ffeb7475b0ff2f405cd3fc6539b56c6e93bfce7
Natalia <nasozontova@gmail.com>
2021-03-26 20:54:48 -0400

    corrected server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Connecting to mongoDB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/myFirstDatabase";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+
+// Connecting routes
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+// Starts the server and begin listening
+app.listen(PORT, () => {
+    console.log(`App listening on Port ${PORT}!`);
+});
